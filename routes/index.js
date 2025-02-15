abee1b0d0070e23837af9c16e541ae0bb353f4b7
Gekyume11 <dhruvharsoda123@gmail.com>
2025-02-15 22:17:57 +0530

    res.send error fix

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
 var express = require('express');
-const User = require('../models/user');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let allUsers;
-  try {
-    allUsers = await User.find();
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-  res.render('index', { title: 'User Form', users: allUsers });
+  res.send("Hello from Server, This is free APIs 4 students by Dhruv!");
 });
 
 module.exports = router;
